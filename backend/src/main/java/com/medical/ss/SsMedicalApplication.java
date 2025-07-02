c6745069c08037a34e71accdb81299e2f9ac4053
soorya <sooryakanagaraj11@gmail.com>
2025-07-02 19:34:53 +0530

    base architecture

---
+++ backend/src/main/java/com/medical/ss/SsMedicalApplication.java
@@ -0,0 +1,13 @@
+package com.medical.ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsMedicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsMedicalApplication.class, args);
+	}
+
+}
